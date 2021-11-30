1e28d5f852c727a3e129f18bdc0c6042606278ac
wasim-eb <Wasse.elb@gmail.com>
2021-11-30 00:04:32 +0000

    redo

--- run.py
+++ run.py
@@ -22,6 +22,9 @@ def placePlayerShips():
     column = input("Please, enter a column to place your ship on, 0-4:")
     playerBoard[row][column] = "@"
 
+#Konvertera input till en siffra
+#Innan du placerar "@", kolla att det inte redan finns ett "@" där.
+
 
 placePlayerShips()
 

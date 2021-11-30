c5b8152a44120f3ad113db7064b83cef23540572
wasim-eb <Wasse.elb@gmail.com>
2021-11-30 00:04:15 +0000

    Inserted the first callings and definition, most of the basics I think so far

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+print("Welcome to Battleshipz")
+
+username = input("Enter Username:")
+
+playerBoard = [[".", ".", ".",".","."], 
+               [".", ".", ".",".","."], 
+               [".", ".", ".",".","."], 
+               [".", ".", ".",".","."], 
+               [".", ".", ".",".","."]]
+
+computerBoard = [[".", ".", ".",".","."], 
+                 [".", ".", ".",".","."], 
+                 [".", ".", ".",".","."], 
+                 [".", ".", ".",".","."], 
+                 [".", ".", ".",".","."]]
+
+
+def placePlayerShips():
+    row = input("Please, enter a row to place your ship on, 0-4:")
+    column = input("Please, enter a column to place your ship on, 0-4:")
+    playerBoard[row][column] = "@"
+
+
+placePlayerShips()
+
+placeComputerShips()
+
+runGame()

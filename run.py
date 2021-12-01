3d49da5ca7e9f8b456a08f4d5a6e04fd6decbee1
wasim-eb <Wasse.elb@gmail.com>
2021-12-01 19:56:21 +0000

    Added loops to place players/computer ships

--- run.py
+++ run.py
@@ -10,22 +10,51 @@ playerBoard = [[".", ".", ".",".","."],
                [".", ".", ".",".","."], 
                [".", ".", ".",".","."]]
 
-computerBoard = [[".", ".", ".",".","."], 
-                 [".", ".", ".",".","."], 
-                 [".", ".", ".",".","."], 
-                 [".", ".", ".",".","."], 
-                 [".", ".", ".",".","."]]
+computerBoard = [[".", ".", ".", ".", ".",], 
+                 [".", ".", ".", ".", "."], 
+                 [".", ".", ".", ".", "."], 
+                 [".", ".", ".", ".", "."], 
+                 [".", ".", ".", ".", "."]]
 
 
 def placePlayerShips():
-    row = int(input("Please, enter a row to place your ship on, 0-4:"))
-    column = int(input("Please, enter a column to place your ship on, 0-4:"))
-    playerBoard[row][column] = "@"
-    if playerBoard = "."
-        print("Your ship has been placed")
-    else playerBoard = "@"
-        print("Error, a ship has already been placed there")
+    for x in range(4):
+        row = int(input("Please, enter a row to place your ship on, 0-4:"))
+        column = int(input("Please, enter a column to place your ship on, 0-4:"))
     
+
+        if playerBoard[row][column] == ".":
+            playerBoard[row][column] = "@"
+            print("Your ship has been placed")
+
+        else:
+            print("Error, a ship has already been placed there")
+    #Lägg till value error?
+        
+    
+def placeComputerShips():
+    for x in range(4):
+        row = randint(0, 4)
+        column = randint(0, 4)
+
+        if computerBoard[row][column] == ".":
+            computerBoard[row][column] = "@"
+
+        else:
+            x = x-1
+            
+
+
+
+def playerGuess():
+    guessRow = int(input("Guess row:"))
+    guessColumn = int(input("Guess column:"))
+
+
+
+
+
+def runGame()
 
 #Konvertera input till en siffra
 #Innan du placerar "@", kolla att det inte redan finns ett "@" där.
